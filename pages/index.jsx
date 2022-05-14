51f837be6606500416fb93e78f95d1964d480baf
Andre Gama del Otero <andredelotero@gmail.com>
2022-05-14 19:46:45 -0300

    description

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,12 +6,10 @@ export default function Home() {
     <>
       <Head>
         <title>Andre Gama del Otero - Front-end developer</title>
-        <description>
-          I am a Frontend developer, living in Mar del Plata, Argentina. I care
-          about site speed, user experience, cross browser compatibility, and of
-          course, responsiveness (I always start projects in a mobile-first
-          approach)
-        </description>
+        <meta
+          name="description"
+          content="I am a Frontend developer, living in Mar del Plata, Argentina. I care about site speed, user experience, cross browser compatibility, and of course, responsiveness (I always start projects in a mobile-first approach)."
+        />
         <link rel="icon" href="/favicon.png" />
       </Head>
       <Navbar />
