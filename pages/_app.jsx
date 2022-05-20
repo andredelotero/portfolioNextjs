94ea7f2af721ee73b942dfe73dd7606939de31d7
Andre Gama del Otero <andredelotero@gmail.com>
2022-05-20 15:40:26 -0300

    font display: block and preload

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,21 +7,21 @@ const GlobalStyle = createGlobalStyle`
     src: url('/fonts/montserrat-v24-latin-300.woff') format('woff');
     font-style: normal;
     font-weight: 300;
-    font-display: swap;
+    font-display: block;
   }
     @font-face {
     font-family: 'Montserrat';
     src: url('/fonts/montserrat-v24-latin-regular.woff') format('woff');
     font-style: normal;
     font-weight: 400;
-    font-display: swap;
+    font-display: block;
   }
     @font-face {
     font-family: 'Montserrat';
     src: url('/fonts/montserrat-v24-latin-500.woff') format('woff');
     font-style: normal;
     font-weight: 500;
-    font-display: swap;
+    font-display: block;
   }
 
   @font-face {
@@ -29,29 +29,9 @@ const GlobalStyle = createGlobalStyle`
     src: url('/fonts/montserrat-v24-latin-700.woff') format('woff');
     font-style: normal;
     font-weight: 700;
-    font-display: swap;
+    font-display: block;
   }
-
 `;
-
-export function reportWebVitals(metric) {
-  const url = process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT;
-
-  if (!url) {
-    return;
-  }
-
-  const body = JSON.stringify({
-    route: window.__NEXT_DATA__.page,
-    ...metric,
-  });
-
-  if (navigator.sendBeacon) {
-    navigator.sendBeacon(url, body);
-  } else {
-    fetch(url, { body, method: "POST", keepalive: true });
-  }
-}
 
 const MyApp = ({ Component, pageProps }) => {
   return (
