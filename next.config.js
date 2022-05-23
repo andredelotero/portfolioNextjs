acdacfef0669c096be6c6df888d90cd1a58b0bd4
Andre Gama del Otero <andredelotero@gmail.com>
2022-05-22 23:36:59 -0300

    cache control for images and fonts

--- next.config.js
+++ next.config.js
@@ -2,4 +2,18 @@ const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   i18n,
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png|woff)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, immutable, max-age=31536000",
+          },
+        ],
+      },
+    ];
+  },
 };
