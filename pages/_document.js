20d69455799fc69d36966cda156b0f8fedb5d85d
Andre Gama del Otero <andredelotero@gmail.com>
2022-06-22 17:14:20 -0300

    display swap on fonts

--- pages/_document.js
+++ pages/_document.js
@@ -34,7 +34,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
